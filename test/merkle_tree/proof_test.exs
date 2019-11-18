a54308d0f551023532f614139f9abd78c33766ce
Pawel Gebal <pawel.gebal@imapp.pl>
2019-11-18 13:31:47 +0100

    feat: prevent second preimage attack

--- test/merkle_tree/proof_test.exs
+++ test/merkle_tree/proof_test.exs
@@ -4,6 +4,8 @@ defmodule MerkleTree.ProofTest do
 
   import MerkleTree.Proof
 
+  @node_salt <<1>>
+
   test "correct proofs" do
     blocks = ~w/a b c d e f g h/
     tree = MerkleTree.new(blocks)
@@ -17,23 +19,6 @@ defmodule MerkleTree.ProofTest do
            |> Enum.with_index()
            |> Enum.zip(proofs)
            |> Enum.map(fn {x, proof} -> proven?(x, tree.root.value, tree.hash_function, proof) end)
-           |> Enum.all?()
-  end
-
-  # TODO: remove when deprecated MerkleTree.Proof.proven?/3 support ends
-  test "correct proofs with deprecated proven?/3" do
-    blocks = ~w/a b c d e f g h/
-    tree = MerkleTree.new(blocks)
-
-    proofs =
-      blocks
-      |> Enum.with_index()
-      |> Enum.map(fn {_, idx} -> prove(tree, idx) end)
-
-    assert blocks
-           |> Enum.with_index()
-           |> Enum.zip(proofs)
-           |> Enum.map(fn {x, proof} -> proven?(x, tree.root.value, proof) end)
            |> Enum.all?()
   end
 
@@ -77,48 +62,18 @@ defmodule MerkleTree.ProofTest do
     assert not proven?({"z", 5}, tree.root.value, tree.hash_function, fake_proof)
   end
 
-  # TODO: remove when deprecated MerkleTree.Proof.proven?/3 support ends
-  test "incorrect proof with deprecated proven?/3" do
-    blocks = ~w/a b c d e f g h/
-    tree = MerkleTree.new(blocks)
-    %MerkleTree.Proof{hashes: hashes} = proof = prove(tree, 5)
+  test "can not prove that node is a leaf" do
+      blocks = ~w/a b c d e f g h/
+      tree = MerkleTree.new(blocks)
 
-    # test sanity
-    assert proven?({"f", 5}, tree.root.value, proof)
+      %MerkleTree.Proof{hashes: hashes} = prove(tree, 0)
+      node_proof = %MerkleTree.Proof{
+        hashes: tl(hashes),
+        hash_function: tree.hash_function
+      }
 
-    # bad index
-    assert not proven?({"f", 6}, tree.root.value, proof)
+      node = tree.hash_function.(@node_salt <> "a" <> "b")
 
-    incomplete_proof = %MerkleTree.Proof{
-      hashes: tl(hashes),
-      hash_function: tree.hash_function
-    }
-
-    assert not proven?({"f", 5}, tree.root.value, incomplete_proof)
-
-    different_hash = %MerkleTree.Proof{
-      hashes: hashes,
-      hash_function: &MerkleTree.Crypto.hash(&1, :sha224)
-    }
-
-    assert not proven?({"f", 5}, tree.root.value, different_hash)
-
-    corrupted_proof = %MerkleTree.Proof{
-      hashes: [tree.hash_function.("z")] ++ tl(hashes),
-      hash_function: tree.hash_function
-    }
-
-    assert not proven?({"f", 5}, tree.root.value, corrupted_proof)
-
-    # corrupted root hash
-    assert not proven?({"f", 5}, tree.hash_function.("z"), proof)
-
-    # fake proof accepted with deprecated proven?/3
-    fake_proof = %MerkleTree.Proof{
-      hashes: Enum.map(hashes, fn _ -> "" end),
-      hash_function: fn _ -> tree.root.value end
-    }
-
-    assert proven?({"z", 5}, tree.root.value, fake_proof)
+      assert not proven?({node, 0}, tree.root.value, tree.hash_function, node_proof)
   end
 end
